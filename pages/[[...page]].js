73624ef42372165435494f755e7e3ecb48e090dc
MarcLlobet <marc.llobet@typeform.com>
2021-10-14 01:41:40 +0200

    next site initial build 2

---
+++ pages/[[...page]].js
@@ -0,0 +1,39 @@
+import { API_URL } from './config'
+
+const Page = ({ slug, blocks, meta }) => (
+  <>
+    <h1>{blocks.title.en}</h1>
+    <p>{blocks.text.en}</p>
+  </>
+)
+
+export async function getStaticProps(context) {
+  const [page=''] = context.params.page || []
+
+  try {
+    const pageProps = await fetch(`${API_URL}/page/${page}`)
+    const props = await pageProps.json()
+    
+    return {
+      props
+    }
+  } catch (error) {
+    return {
+      notFound: true
+    }
+  }
+}
+  
+export async function getStaticPaths(context) {
+  const pagePathsResponse = await fetch(`${API_URL}/page/_paths`)
+  const pagePaths = await pagePathsResponse.json()
+
+  const paths = pagePaths.map(path => ({ params: { page: [path] } }))
+
+  return {
+    paths,
+    fallback: 'blocking'
+  }
+}
+
+export default Page
